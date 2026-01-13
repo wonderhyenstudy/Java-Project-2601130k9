ebeca7dd9d225294fc3260e732cd0eb2adcdea3e
lsy3709 <lsy3709@naver.com>
2026-01-13 10:48:04 +0900

    first commit

---
+++ src/_1_test_260113/_0_HelloWorld.java
@@ -0,0 +1,8 @@
+package _1_test_260113;
+
+public class _0_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("오늘 점심 뭐 먹지?");
+    }
+}
