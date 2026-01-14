2b7c25e2319f57ba37f81a4664baa25ed7cfdac9
lsy3709 <lsy3709@naver.com>
2026-01-14 09:24:42 +0900

    260114, 1교시, 복습 중,

---
+++ src/_2_test_260114/ex/_0_Test_Condition_Ex.java
@@ -0,0 +1,11 @@
+package _2_test_260114.ex;
+
+public class _0_Test_Condition_Ex {
+    // 순서1, main -> 코드 스니펫으로 메인 메서드 자동 생성
+    public static void main(String[] args) {
+        // 순서2, sout -> 코드 스니펫으로 출력 메서드 자동 생성
+        // 순서3, 실행 , 단축키 : ctrl + shift + f10
+        // or , 클릭 플레이 버튼
+        System.out.println("수업 준비 확인");
+    }
+}
